9fbeb6727aa882ec2d7b4818ae728fa5b1eba1a9
Garrett Wu <gwu@likelihood.com>
2016-08-27 10:23:03 -0400

    First version of the grammer without calculations

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -13,8 +13,67 @@ describe('index', () => {
       })
     })
 
-    it('parses the letter "a"', function () {
-      expect(this.parser.parse('a')).toBe('a')
+    it('parses the empty string', function () {
+      expect(this.parser.parse('')).toEqual('')
+    })
+
+    it('parses "abc"', function () {
+      expect(this.parser.parse('abc')).toEqual('abc')
+    })
+
+    it('parses "123"', function () {
+      expect(this.parser.parse('123')).toEqual('123')
+    })
+
+    it('parses "123px"', function () {
+      expect(this.parser.parse('123px')).toEqual('123px')
+    })
+
+    it('parses "{{products[1].name}}"', function () {
+      expect(this.parser.parse('{{products[1].name}}')).toEqual({
+        type: 'ref',
+        name: 'products',
+        rank: 1,
+        field: 'name'
+      })
+    })
+
+    it('parses "Cool {{products[1].category}}!"', function () {
+      expect(this.parser.parse('Cool {{products[1].category}}!')).toEqual([
+        'Cool ',
+        {
+          type: 'ref',
+          name: 'products',
+          rank: 1,
+          field: 'category'
+        },
+        '!'
+      ])
+    })
+
+    it('parses "A {{products[3].name}} is in {{products[3].category}}"', function () {
+      expect(this.parser.parse('A {{products[3].name}} is in {{products[3].category}}')).toEqual([
+        'A ',
+        {
+          type: 'ref',
+          name: 'products',
+          rank: 3,
+          field: 'name'
+        },
+        ' is in ',
+        {
+          type: 'ref',
+          name: 'products',
+          rank: 3,
+          field: 'category'
+        }
+      ])
+    })
+
+    it('parses "A literal {{that is not valid code}}"', function () {
+      expect(this.parser.parse('A literal {{that is not valid code}}')).toEqual(
+        'A literal {{that is not valid code}}'
+      )
     })
   })
 })
--- src/index.pegjs
+++ src/index.pegjs
@@ -1,4 +1,53 @@
-start = letter
+Start = CreativePropertyExpression
 
-letter = "a"
+CreativePropertyExpression = first : StringLiteral rest : EmbeddedCodeFollowedByStringLiteral * {
+  const nested = [first === '' ? [] : first].concat(rest)
+  const flattened = nested.reduce((f, e) => f.concat(e), [])
+  if (flattened.length === 0) {
+    return ''
+  }
+  if (flattened.length === 1) {
+    return flattened[0]
+  }
+  return flattened
+}
 
+StringLiteral = chars : CharacterLiteral * {
+  return chars.join('')
+}
+
+CharacterLiteral = ! EmbeddedCode char : . {
+  return char
+}
+
+EmbeddedCodeFollowedByStringLiteral = first : EmbeddedCode second : StringLiteral {
+  if (second === '') {
+    return first
+  }
+  return [first, second]
+}
+
+EmbeddedCode = "{{" code : Code "}}" { return code }
+
+Code = DataRef
+
+DataRef = query : DataRefIdentifier "[" rank : DataRefRank "]." field : DataRefField {
+  return {
+    type: 'ref',
+    name: query,
+    rank: rank,
+    field: field
+  }
+}
+
+DataRefIdentifier = Identifier
+
+Identifier = first : [_a-zA-Z] rest : [_a-zA-Z0-9] * {
+  return [first].concat(rest).join('')
+}
+
+DataRefRank = first : [1-9] rest : [0-9] * {
+  return parseInt([first].concat(rest).join(''))
+}
+
+DataRefField = Identifier
